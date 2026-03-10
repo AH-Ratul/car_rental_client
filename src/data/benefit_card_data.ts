e49a8c7303b08e84df45e7c16475b428b83ea9f8
AH-Ratul <ahratul21@gmail.com>
2026-03-10 15:18:50 +0600

    add sample card data

---
+++ src/data/benefit_card_data.ts
@@ -0,0 +1,33 @@
+import technology from "../../public/images/tech.png";
+import techIcon from "../../public/images/tech1.png";
+import reward from "../../public/images/reward.png";
+import rewardIcon from "../../public/images/reward1.png";
+import comfort from "../../public/images/comfort.png";
+import comfortIcon from "../../public/images/comfort1.png";
+
+export const benefits = [
+  {
+    id: 1,
+    image: technology,
+    icon: techIcon,
+    title: "Technology",
+    description:
+      "Our state of art booking system is safe. And secure enabling to book online. Our online automated check-in features save you time at the rental desk and helps to eliminates paperwork.",
+  },
+  {
+    id: 2,
+    image: reward,
+    icon: rewardIcon,
+    title: "Reward",
+    description:
+      "Our loyalty reward program has been designed specifically to frequent flyers. The more you travel the more points credited to your account which can be redeemed online. Drive more and save more.",
+  },
+  {
+    id: 3,
+    image: comfort,
+    icon: comfortIcon,
+    title: "Comfort",
+    description:
+      "Your comfort is out priority. Our cars well-cleaned and maintained in proper order. We have provided a list of extras to enhance your riding experience.",
+  },
+];
