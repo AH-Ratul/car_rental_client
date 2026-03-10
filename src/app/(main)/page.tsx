f49f24c488d9003117a0dc3bbf01b91e1878b445
AH-Ratul <ahratul21@gmail.com>
2026-03-10 15:22:37 +0600

    feat: add benefit card section

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,13 +1,19 @@
-import Image from "next/image";
 import HeroSection from "./_ui/HeroSection";
 import BenefitSection from "./_ui/BenefitSection";
+import VehicleCard from "@/src/components/shared/VehicleCard";
+import ExploreCarSetion from "./_ui/ExploreCarSection";
+import BenefitCard from "./_ui/BenefitCard";
 
 export default function Home() {
   return (
     <div className="w-full">
       <HeroSection />
       <BenefitSection />
-      
+
+      <div className="flex justify-center items-center px-20  mt-20 w-full bg-gray-100 py-20">
+        <ExploreCarSetion />
+      </div>
+        <BenefitCard />
     </div>
   );
 }
